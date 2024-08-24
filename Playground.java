9474155a93f7a5e978086e7aece9ae6b1598d41a
Asa Wang <axxwang13@gmail.com>
2024-08-24 13:04:00 -0400

    Initial commit programming workshop

---
+++ Playground.java
@@ -0,0 +1,17 @@
+public class Playground {
+    public static void main(String[] args) {
+        /* Put your code below here! */
+
+        final String name = "Matthew";
+        int age = 17;
+        boolean areRobotsCool = true;
+        final double headsProbability = 0.50;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(areRobotsCool);
+        System.out.println(headsProbability);
+
+        /* ... and above here! */
+    }
+}
